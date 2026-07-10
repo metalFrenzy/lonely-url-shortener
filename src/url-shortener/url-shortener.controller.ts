328e117707c30e7c718c5b9e413b999b91dfc88b
metalFrenzy <ahmed.0881100@gmail.com>
2026-07-11 02:50:25 +0300

    register controllers and inject into the module

--- src/url-shortener/url-shortener.controller.ts
+++ src/url-shortener/url-shortener.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { UrlShortenerService } from './url-shortener.service';
+import { ShortenUrlDto } from 'src/dtos/shorten-url.dto';
+import { ShortenUrlResponseDto } from 'src/dtos/shorten-url-response.dto';
+import type { Response } from 'express';
 
 @Controller('url-shortener')
-export class UrlShortenerController {}
+export class UrlShortenerController {
+  constructor(private readonly urlService: UrlShortenerService) {}
+
+  @Post('shorten')
+  async shorten(@Body() dto: ShortenUrlDto): Promise<ShortenUrlResponseDto> {
+    const url = await this.urlService.shorten(dto.originalUrl);
+    return {
+      originalUrl: url.originalUrl,
+      shortCode: url.shortCode,
+    };
+  }
+
+  @Get(':code')
+  async redirect(@Param('code') code: string, @Res() res: Response) {
+    const url = await this.urlService.findByShortCode(code);
+    return res.redirect(HttpStatus.FOUND, url.originalUrl);
+  }
+}
--- src/url-shortener/url-shortener.module.ts
+++ src/url-shortener/url-shortener.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlShortenerController } from './url-shortener.controller';
 import { UrlShortenerService } from './url-shortener.service';
+import { Url } from 'src/entities/url.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Url])],
   controllers: [UrlShortenerController],
   providers: [UrlShortenerService],
 })
