08a144b3c74d68e2b8c06d1f7508f4b67a8f7e6e
metalFrenzy <ahmed.0881100@gmail.com>
2026-07-11 02:50:38 +0300

    update the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips any properties not defined in the DTO
+      transform: true, // auto-converts payloads into DTO class instances
+    }),
+  );
+  await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
